4dbc09df85eb3380ef610aa45f6b7685f9b286e9
mathilde couraud <mcouraud13@gmail.com>
2020-01-14 14:01:34 +0100

    front-end done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Author < ApplicationRecord
-  has_many :quotes
+  has_many :quotes, dependent: :destroy
   validates :first_name, :last_name, presence: true
 end
