248b1b00180324a340c1bc125ce95b55b0fcfc92
mathilde couraud <mcouraud13@gmail.com>
2020-01-14 16:34:20 +0100

    tests being done

--- spec/controllers/quotes_controller_spec.rb
+++ spec/controllers/quotes_controller_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 RSpec.describe QuotesController, type: :controller do
   describe "GET show" do
-    it "is expected to return a random quote from the params[:author_id]" do
+    it "is expected to return a random quote from the selected author" do
 
     end
 
     it "renders the show template" do
-      get :show
+      @author = Author.create(first_name: "Woody", last_name: "Allen")
+      sentence = "Eighty percent of success is showing up."
+      @quote = Quote.create(author: @author, sentence: sentence)
+      get :show, quote_id: @quote.id
       expect(response).to render_template("show")
     end
   end
